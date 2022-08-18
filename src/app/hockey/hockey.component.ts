ac72cead738c19323b34cfa05be31fcfb5ba1709
JunekCZ <junekojenda@seznam.cz>
2022-08-18 19:22:03 +0200

    Displaying shots

--- src/app/hockey/hockey.component.ts
+++ src/app/hockey/hockey.component.ts
@@ -10,7 +10,7 @@ export class HockeyComponent implements OnInit {
   token: string = "";
   competition: string = "";
   teamUuid: string = "";
-  shotArray: iShot[] = [];
+  shotsArray: iShot[] = [];
 
   constructor(private shotsService: ShotsService) {
     this.token = "8bda9a4f6490c8602322ad3d36305ce2103cb34b";
@@ -19,12 +19,21 @@ export class HockeyComponent implements OnInit {
     // Rytíři Kladno
     this.teamUuid = "42bafa4d-5e60-4f00-81c5-4f44ffa55927";
     shotsService.getShotsData(this.token, this.competition, this.teamUuid).subscribe(data => {
-      this.shotArray = data;
-      console.log(this.shotArray);
+      this.shotsArray = data;
+      this.shotsArray = this.shotsArray.filter(shot => shot.x > 22 && shot.x < 101 && shot.y > -101 && shot.y < 101);
+      this.computeDots();
+      console.log(this.shotsArray);
     });
   }
 
   ngOnInit(): void {
+  }
+
+  computeDots(): void {
+    this.shotsArray.forEach(element => {
+      element.computedX = (element.y * -1 + 100) / 200 * 100,
+      element.computedY = 100 - (element.x - 23) / 77 * 100;
+    });
   }
 
 }
